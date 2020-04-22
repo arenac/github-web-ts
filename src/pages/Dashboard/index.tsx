5fc4c5315875d879f8a3f562065ab7277a5b7261
Nilo Neregato <neregato.nilo@gmail.com>
2020-04-22 22:46:16 +0200

    fetch data from api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,54 +1,61 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImage from '../../assets/logo.svg';
 
 import { Title, Form, Reposiories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 const Dashboard: React.FC = () => {
+  const [reposistories, setRepositories] = useState<Repository[]>([]);
+  const [newRepo, setNewRepo] = useState('');
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...reposistories, repository]);
+    setNewRepo('');
+  };
+
   return (
     <>
       <img src={logoImage} alt="Github Explorer" />
       <Title>Explore Github repositories</Title>
 
-      <Form action="">
-        <input placeholder="Enter the repo name" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Enter the repo name"
+        />
         <button type="submit">Search</button>
       </Form>
       <Reposiories>
-        <a href="/">
+        {reposistories.map(repository => (
+          <a key={repository.full_name} href="/">
           <img
-            src="https://avatars3.githubusercontent.com/u/2737134?s=460&u=48ab411bf8f3dcb74a3dfa57110ec92762c0b578&v=4"
-            alt="nilo"
+            src={repository.owner.avatar_url}
+            alt={repository.owner.login}
           />
           <div>
-            <strong>github-web-ts</strong>
-            <p>this project was bootstrapped with Create React App.</p>
+            <strong>{repository.full_name}</strong>
+            <p>{repository.description}</p>
           </div>
           <FiChevronRight size={20} />
         </a>
-        <a href="/">
-          <img
-            src="https://avatars3.githubusercontent.com/u/2737134?s=460&u=48ab411bf8f3dcb74a3dfa57110ec92762c0b578&v=4"
-            alt="nilo"
-          />
-          <div>
-            <strong>github-web-ts</strong>
-            <p>this project was bootstrapped with Create React App.</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="/">
-          <img
-            src="https://avatars3.githubusercontent.com/u/2737134?s=460&u=48ab411bf8f3dcb74a3dfa57110ec92762c0b578&v=4"
-            alt="nilo"
-          />
-          <div>
-            <strong>github-web-ts</strong>
-            <p>this project was bootstrapped with Create React App.</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        ))}
       </Reposiories>
     </>
   );
